001e84d77ac6f7dbfd53b4b9301052e9714ce4f0
Francisco <fcisco.dev@gmail.com>
2022-01-03 20:20:39 +0700

    condigure route on main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
-from .router import general
+from app.router import day
+
+
+from .router import general, year, month
 from .dependencies import DATA
 
 app = FastAPI()
 
 app.include_router(general.router)
-
-
-@app.get("/data")
-def data():
-    return DATA
+app.include_router(year.router)
+app.include_router(month.router)
+app.include_router(day.router)
